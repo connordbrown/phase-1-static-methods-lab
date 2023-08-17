5b58d53c3e8b4f849b6a79461492691ba59c37e5
Connor <connordbrown@gmail.com>
2023-08-16 18:13:16 -0600

    completes lab

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(word) {
+    return word.slice(0,1).toUpperCase() + word.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+  static titleize(string) {
+    const checkCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let strArr = string.split(" ")
+    let newArr = [];
+    newArr.push(this.capitalize(strArr[0]));
+    for (let word of strArr.slice(1)) {
+      if (checkCap.includes(word)) {
+        newArr.push(word);
+      } else {
+        newArr.push(this.capitalize(word));
+      }
+    }
+
+    return newArr.join(" ");
+  }
+
 }
+
+let str = "chicken soup with rice and a few other songs"
+let sts = "a tale of two cities"
+console.log(Formatter.titleize(str));
+console.log(Formatter.titleize(sts));
